e4469f48c6211ddade440ed95d76b5c84ac88ee5
arusrex <arusbr@hotmail.com>
2024-05-22 15:40:29 -0300

    att

--- project/project/settings.py
+++ project/project/settings.py
@@ -129,6 +129,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = DATA_DIR / 'static'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media'),
+]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = DATA_DIR / 'media'
 
