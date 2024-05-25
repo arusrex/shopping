880a7a57fc9e09f3662c8e11ec4003bd46569380
arusrex <arusbr@hotmail.com>
2024-05-25 15:50:24 -0300

    att

--- project/site_setup/models.py
+++ project/site_setup/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from utils.model_validators import validate_png
-from utils.images import resize_image
+from utils.images import resize_image, resize_image_slide
 
 class MenuLink(models.Model):
     class Meta:
@@ -29,19 +29,48 @@ class SiteSetup(models.Model):
     show_description = models.BooleanField(default=True)
     show_pagination = models.BooleanField(default=True)
     show_footer = models.BooleanField(default=True)
+    show_carousel = models.BooleanField(default=True)
+
+    slide1 = models.ImageField(upload_to='assets/slides/%Y/%m/%d/', blank=True, default='', validators=[validate_png])
+    slide2 = models.ImageField(upload_to='assets/slides/%Y/%m/%d/', blank=True, default='', validators=[validate_png])
+    slide3 = models.ImageField(upload_to='assets/slides/%Y/%m/%d/', blank=True, default='', validators=[validate_png])
 
     favicon = models.ImageField(upload_to='assets/favicon/%Y/%m/%d/', blank=True, default='', validators=[validate_png])
 
     def save(self, *args, **kwargs):
         current_favicon_name =  str(self.favicon.name)
+        current_slide1_name = str(self.slide1.name)
+        current_slide2_name = str(self.slide2.name)
+        current_slide3_name = str(self.slide3.name)
         super().save(*args, **kwargs)
         favicon_changed = False
+        slide1_changed = False
+        slide2_changed = False
+        slide3_changed = False
+
+        if self.slide1:
+            slide1_changed = current_slide1_name != self.slide1.name
+
+        if self.slide2:
+            slide2_changed = current_slide2_name != self.slide2.name
+
+        if self.slide3:
+            slide3_changed = current_slide3_name != self.slide3.name
 
         if self.favicon:
             favicon_changed = current_favicon_name != self.favicon.name
 
         if favicon_changed:
             resize_image(self.favicon, 32)
+        
+        if slide1_changed:
+            resize_image_slide(self.slide1)
+
+        if slide2_changed:
+            resize_image_slide(self.slide2)
+
+        if slide3_changed:
+            resize_image_slide(self.slide3)
 
     def __str__(self):
         return self.title
