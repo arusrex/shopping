e89ae5eaf85ec4ff13cdc807253ad7586135ee27
arusrex <arusbr@hotmail.com>
2024-05-31 09:42:18 -0300

    att

--- project/blog/views/index_views.py
+++ project/blog/views/index_views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 def index(request):
+
+    # item_list = Item.objects.all()
+
+    # paginator = Paginator(iteem_list, 9)
+    # page_number = request.GET.get('page')
+    # page_obj = paginator.get_page(page_number)
+
+    # context = {
+    #     'page_obj': page_obj,
+    # }
+
     return render(request, 'blog/pages/index.html')
