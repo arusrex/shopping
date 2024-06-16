0a0fe0ff5f3557adccc27f76b1c5f55950f62b24
arusrex <arusbr@hotmail.com>
2024-06-16 09:46:54 -0300

    "Added redirect to site_config view, changed SiteSetup instance retrieval to use get_or_create, and uncommented import of redirect function."

--- project/site_setup/views.py
+++ project/site_setup/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from site_setup.forms import SiteSetupForm
 from django.contrib import messages
 from site_setup.models import SiteSetup
 
 def site_config(request):
-    site_setup_instance = SiteSetup.objects.get()
+    # site_setup_instance = SiteSetup.objects.get()
+    site_setup_instance, created = SiteSetup.objects.get_or_create(id=1)
 
     if request.method == 'POST':
         form = SiteSetupForm(request.POST, request.FILES, instance=site_setup_instance)
@@ -12,6 +13,7 @@ def site_config(request):
         if form.is_valid():
             form.save()
             messages.success(request, "Configurações atualizadas com sucesso")
+            return redirect('blog:site_config')
     else:
         form = SiteSetupForm(instance=site_setup_instance)
 
